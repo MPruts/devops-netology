706f2860b8b0463af8f3260c456080dc0ecfb3c1
Mikle <prumyu@mail.ru>
2021-12-05 22:40:36 +0500

    07-terraform-02-syntax

---
+++ 07-terraform-02-syntax/terraform/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_region" "mytestregion" {}
+
+data "aws_caller_identity" "mytestcalleridentity" {}
+
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Mikhail"
+    Lesson = "Netology"
+    region = data.aws_region.mytestregion.name
+  }
+
+  user_data = <<EOF
+apt update
+app upgrade -y
+EOF
+
+}
